dc3a872724814d046d35caab8947d0f37dbdd09c
Daniel25TE <danielalejandrosud25@gmail.com>
2025-12-03 10:06:55 -0700

    autobiography collection and search endpoint stretch challenge test 1

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const router = require('express').Router();
 router.use('/', require('./auth'));
 
 // Collections
-//router.use('/autobiography', require('./autobiography')); 
+router.use('/autobiography', require('./autobiographyRoute')); 
 router.use('/fantasy', require('./fantasyRoute'));
 router.use('/mystery', require('./mysteryRoute'));
 router.use('/romance', require('./romanceRoute'));
 router.use('/users', require('./users'));
+router.use('/search', require('./searchRoute'));
 
 router.use('/', require('./swagger'));
 
